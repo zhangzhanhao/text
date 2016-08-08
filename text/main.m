444b288a4330f1b71b6ddea7155e2f1a02e5558f
lanou <lanou@zhangzhanhao.local>
2016-08-08 09:51:29 +0800

    Initial Commit

---
+++ text/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  text
+//
+//  Created by lanou on 16/8/8.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
